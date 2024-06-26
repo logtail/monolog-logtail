4c9cc6575b2a6c7ff6695cab0640308a27128fe8
Petr Heinz <petr@betterstack.com>
2024-06-26 16:58:45 +0200

    Add option to flush in time intervals (default every second)

--- src/Monolog/LogtailHandler.php
+++ src/Monolog/LogtailHandler.php
@@ -19,15 +19,28 @@ use Monolog\Logger;
  */
 class LogtailHandler extends BufferHandler
 {
+    const DEFAULT_ALWAYS_FLUSH_AFTER_MILLISECONDS = 1000;
+
+    /**
+     * @var int|null $alwaysFlushAfterMs
+     */
+    private $alwaysFlushAfterMs;
+
+    /**
+     * @var int|float|null highResolutionTimeOfNextFlush
+     */
+    private $highResolutionTimeOfNextFlush;
+
     /**
      * @param string        $sourceToken            Logtail source token
      * @param int|string    $level                  The minimum logging level at which this handler will be triggered
      * @param bool          $bubble                 Whether the messages that are handled can bubble up the stack or not
-     * @param               $endpoint               Logtail ingesting endpoint
+     * @param string        $endpoint               Logtail ingesting endpoint
      * @param int           $bufferLimit            How many entries should be buffered at most, beyond that the oldest items are removed from the buffer.
      * @param bool          $flushOnOverflow        If true, the buffer is flushed when the max size has been reached, by default oldest entries are discarded
      * @param int           $connectionTimeoutMs    The maximum time in milliseconds that you allow the connection phase to the server to take
      * @param int           $timeoutMs              The maximum time in milliseconds that you allow a transfer operation to take
+     * @param int|null      alwaysFlushAfterMs      The time in milliseconds after which next log record will trigger flushing all logs. Null to disable.
      */
     public function __construct(
         $sourceToken,
@@ -37,9 +50,39 @@ class LogtailHandler extends BufferHandler
         $bufferLimit = 0,
         $flushOnOverflow = false,
         $connectionTimeoutMs = LogtailClient::DEFAULT_CONNECTION_TIMEOUT_MILLISECONDS,
-        $timeoutMs = LogtailClient::DEFAULT_TIMEOUT_MILLISECONDS
+        $timeoutMs = LogtailClient::DEFAULT_TIMEOUT_MILLISECONDS,
+        $alwaysFlushAfterMs = self::DEFAULT_ALWAYS_FLUSH_AFTER_MILLISECONDS
     ) {
         parent::__construct(new SynchronousLogtailHandler($sourceToken, $level, $bubble, $endpoint, $connectionTimeoutMs, $timeoutMs), $bufferLimit, $level, $bubble, $flushOnOverflow);
+        $this->alwaysFlushAfterMs = $alwaysFlushAfterMs;
+        $this->setHighResolutionTimeOfLastFlush();
     }
 
+    /**
+     * @inheritDoc
+     */
+    public function handle(array $record): bool
+    {
+        $return = parent::handle($record);
+
+        if ($this->highResolutionTimeOfNextFlush !== null && $this->highResolutionTimeOfNextFlush <= hrtime(true)) {
+            $this->flush();
+            $this->setHighResolutionTimeOfLastFlush();
+        }
+
+        return $return;
+    }
+
+    private function setHighResolutionTimeOfLastFlush(): void
+    {
+        $currentHighResolutionTime = hrtime(true);
+        if ($this->alwaysFlushAfterMs === null || $currentHighResolutionTime === false) {
+            $this->highResolutionTimeOfNextFlush = null;
+
+            return;
+        }
+
+        // hrtime(true) returns nanoseconds, converting alwaysFlushAfterMs from milliseconds to nanoseconds
+        $this->highResolutionTimeOfNextFlush = $currentHighResolutionTime + $this->alwaysFlushAfterMs * 1e+6;
+    }
 }
